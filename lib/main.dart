26250bb8ac25c1aef137dd95518257c0bbf01cbf
Youngbin Han <ybhan@ubuntu.com>
2023-06-25 23:45:33 +0900

    Convert image to 1bit monochrome

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'dart:typed_data';
 import 'dart:typed_data';
 import 'dart:ui' as ui;
-
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/material.dart';
@@ -38,12 +38,14 @@ class MyAppState extends ChangeNotifier {
     RenderRepaintBoundary boundary =
         globalKey.currentContext!.findRenderObject() as RenderRepaintBoundary;
     ui.Image image = await boundary.toImage();
+
     ByteData? byteData =
         await image.toByteData(format: ui.ImageByteFormat.rawRgba);
-    Uint8List pngBytes = byteData!.buffer.asUint8List();
-    var binaryBitmap = imglib.Image.fromBytes(550, 550, pngBytes)
-        .convert(numChannels: 4, alpha: 255);
-    print(binaryBitmap);
+    var binaryBitmap =
+        imglib.Image.fromBytes(width: 550, height: 200, bytes: byteData!.buffer)
+            .convert(format: imglib.Format.uint1, numChannels: 1)
+            .getBytes();
+    // print(binaryBitmap);
     return binaryBitmap;
   }
 
@@ -64,23 +66,26 @@ class MyAppState extends ChangeNotifier {
     deviceList = _configuration.interfaces[0].endpoints.toString();
     var endpoint = _configuration.interfaces[0].endpoints
         .firstWhere((e) => e.direction == UsbEndpoint.DIRECTION_OUT);
-    var imageUint8 = await _capturePng();
+
+    // var clscmd = utf8.encode("CLS\r\n");
+    // var bulkTransferOutCls = await QuickUsb.bulkTransferOut(
+    //     endpoint, Uint8List.fromList(clscmd),
+    //     timeout: 2000);
+    // print('bulkTransferOutCls $bulkTransferOutCls');
+
     // var cmddata = utf8.encode("CODEPAGE 949");
     var cmddata = utf8.encode("SIZE 70 mm,70 mm\r\n");
     // cmddata += utf8.encode("SIZE 70 mm,70 mm\r\n");
     cmddata += utf8.encode("CLS\r\n");
-    cmddata += utf8.encode('BITMAP 0,0,72,550,1, ');
+    cmddata += utf8.encode('BITMAP 0,0,68,200,0, ');
     // print('image bitmap: $imageUint8');\
-    if (imageUint8 != null) {
-      cmddata += imageUint8;
-    }
+    var imageUint8 = await _capturePng();
+    cmddata += imageUint8;
     cmddata += utf8.encode("PRINT 1\r\n");
     cmddata += utf8.encode("END\r\n");
     // print(utf8.decode(cmddata));
-    var bulkTransferOut = await QuickUsb.bulkTransferOut(
-      endpoint,
-      Uint8List.fromList(cmddata),
-    );
+    var bulkTransferOut =
+        await QuickUsb.bulkTransferOut(endpoint, Uint8List.fromList(cmddata));
     print('bulkTransferOut $bulkTransferOut');
     await QuickUsb.closeDevice();
     notifyListeners();
@@ -112,37 +117,14 @@ class MyHomePage extends StatelessWidget {
               onPressed: () async {
                 appState.updateDeviceList();
               }),
-          RepaintBoundary(
-              key: appState.globalKey,
-              child: SizedBox(
-                  width: 550.0,
-                  height: 550.0,
-                  child: ColorFiltered(
-                    colorFilter: ColorFilter.matrix(<double>[
-                      0.2126,
-                      0.7152,
-                      0.0722,
-                      0,
-                      0,
-                      0.2126,
-                      0.7152,
-                      0.0722,
-                      0,
-                      0,
-                      0.2126,
-                      0.7152,
-                      0.0722,
-                      0,
-                      0,
-                      0,
-                      0,
-                      0,
-                      1,
-                      0,
-                    ]),
-                    child: Center(
-                      child: const Text("라벨 출력 테스트"),
-                    ),
+          SizedBox(
+              width: 550.0,
+              height: 200.0,
+              child: RepaintBoundary(
+                  key: appState.globalKey,
+                  child: Container(
+                    color: Colors.white,
+                    child: Center(child: Text("라벨 출력 테스트")),
                   )))
         ],
       ),
