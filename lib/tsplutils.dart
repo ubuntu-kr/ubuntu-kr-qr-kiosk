f26e1135a34131301c0f6365a77af7d816fdf569
Youngbin Han <ybhan@ubuntu.com>
2024-06-12 02:29:00 +0900

    buildBitmapPrintTsplCmd 문법 오류 수정

--- lib/tsplutils.dart
+++ lib/tsplutils.dart
@@ -6,11 +6,11 @@ Uint8List buildBitmapPrintTsplCmd(int x, int y, int imgWidthPx, int imgHeightPx,
     int canvasWidthMm, int canvasHeightMm, Uint8List imageBitmap) {
   var widthInBytes = (imgWidthPx / 8).ceil();
   var cmddata = utf8.encode("SIZE $canvasWidthMm mm,$canvasHeightMm mm\r\n");
-  cmddata += utf8.encode("CLS\r\n");
-  cmddata += utf8.encode('BITMAP $x,$y,$widthInBytes,$imgHeightPx,1, ');
-  cmddata += imageBitmap;
-  cmddata += utf8.encode("\r\nPRINT 1\r\n");
-  cmddata += utf8.encode("END\r\n");
+  cmddata.addAll(utf8.encode("CLS\r\n"));
+  cmddata.addAll(utf8.encode('BITMAP $x,$y,$widthInBytes,$imgHeightPx,1, '));
+  cmddata.addAll(imageBitmap);
+  cmddata.addAll(utf8.encode("\r\nPRINT 1\r\n"));
+  cmddata.addAll(utf8.encode("END\r\n"));
   return Uint8List.fromList(cmddata);
 }
 
